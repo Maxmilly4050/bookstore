edf2422cccd77988e5bddcb8e43e62d969b488d9
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-18 12:37:20 +0300

    Enhance home view to include book statistics and update index template for improved display

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Book
+from django.db.models import Avg, Count
 
 def home(request):
     books = Book.objects.all()
-    return render(request, 'book_outlet/index.html', {'books': books})
+    num_books = books.count()
+    book_stats = Book.objects.aggregate(avg_price=Avg('price'), total_books=Count('id'))
+    return render(request, 'book_outlet/index.html', {'books': books, 'num_books': num_books, 'book_stats': book_stats})
 
 def about(request):
     return render(request, 'book_outlet/about.html')
