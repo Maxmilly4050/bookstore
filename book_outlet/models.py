5038fb0473951454b4ecc58d107a968bce7bf548
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-18 12:11:00 +0300

    Add slug and description fields to Book model; implement slug generation on save

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -6,6 +7,12 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     stock = models.IntegerField()
+    slug = models.SlugField(default="", null=False, unique=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
