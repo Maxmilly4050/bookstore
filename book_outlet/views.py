0860487d707160f045920c5a9b2b5a5aa2c359df
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-18 12:38:24 +0300

    Sort books by title in home view for improved display

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -3,7 +3,7 @@ from .models import Book
 from django.db.models import Avg, Count
 
 def home(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by('title')
     num_books = books.count()
     book_stats = Book.objects.aggregate(avg_price=Avg('price'), total_books=Count('id'))
     return render(request, 'book_outlet/index.html', {'books': books, 'num_books': num_books, 'book_stats': book_stats})
