161b7ded0387784fc555db44e0a9cb49139d400e
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-19 16:47:29 +0300

    Enhance BookAdmin configuration with additional filters, search fields, and display options for improved admin usability

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -4,6 +4,9 @@ from .models import Book
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('author', 'price')
+    search_fields = ('title', 'author')
+    list_display = ('title', 'author', 'price', 'stock')
 
 
 admin.site.register(Book, BookAdmin)
