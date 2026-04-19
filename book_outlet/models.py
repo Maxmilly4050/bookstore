67cbd47d9590288dc7d6bbb0a7923c9de78f520d
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-19 17:14:53 +0300

    Refactor Book model to change author field from CharField to ForeignKey for improved relational integrity

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,7 +4,7 @@ from django.utils.text import slugify
 class Book(models.Model):
     title = models.CharField(max_length=200)
     rating = models.DecimalField(max_digits=3, decimal_places=2)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     stock = models.IntegerField()
     slug = models.SlugField(default="", null=False, db_index=True, editable=False)
@@ -13,3 +13,9 @@ class Book(models.Model):
     def __str__(self):
         return self.title
     
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
