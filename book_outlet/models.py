5fb5678c579dbc5ccbea4550678e73ff6037d531
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-19 17:25:55 +0300

    Add author as foreign key and make migration

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,7 +4,7 @@ from django.utils.text import slugify
 class Book(models.Model):
     title = models.CharField(max_length=200)
     rating = models.DecimalField(max_digits=3, decimal_places=2)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     stock = models.IntegerField()
     slug = models.SlugField(default="", null=False, db_index=True, editable=False)
