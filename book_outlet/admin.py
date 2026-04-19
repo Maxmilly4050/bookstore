c027d4e15c4e01db9bcd3a626ff4fc2c0cb756b2
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-19 18:00:18 +0300

    Add Author model to admin and update BookAdmin registration

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('author', 'price')
     search_fields = ('title', 'author')
     list_display = ('title', 'author', 'price', 'stock')
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
+
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
