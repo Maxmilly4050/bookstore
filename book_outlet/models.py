71f9168469cc9cf5ff1066b95e0333f50e7392f1
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-19 18:18:48 +0300

    Add Address model and update Author model to include Address as a one-to-one key

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -16,6 +16,16 @@ class Book(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    Address = models.OneToOneField('Address', on_delete=models.CASCADE, null=True, related_name='author')
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.state} {self.zip_code}"
